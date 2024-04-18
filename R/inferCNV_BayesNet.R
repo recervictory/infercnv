b1c4d8048d2070f018bf0b3f2d6f5307b5d2f4b0
Victor Banerjee <recervictory@gmail.com>
2024-04-18 09:14:59 +0000

    Step 18 Issue

--- R/inferCNV_BayesNet.R
+++ R/inferCNV_BayesNet.R
@@ -404,30 +404,80 @@ setGeneric(name="withParallel",
 #' @rdname withParallel-method
 #' @aliases withParallel
 #' @noRd
+# setMethod(f="withParallel",
+#           signature="MCMC_inferCNV",
+#           definition=function(obj)
+#           {
+#               par_func <- function(i){
+#                   if (getArgs(obj)$quietly == FALSE) {
+#                       futile.logger::flog.info(paste("Sampling Number: ", i))
+#                   }
+#                   if(!(length(obj@cell_gene[[i]]$Cells) == 0)){
+#                       tumor_grouping <- obj@group_id[ obj@cell_gene[[i]]$Cells ] # subset the tumor ids for the cells wanted
+#                       gene_exp <- obj@expr.data[obj@cell_gene[[i]]$Genes, obj@cell_gene[[i]]$Cells]
+#                       return(run_gibb_sampling(gene_exp, obj))
+#                   } else {
+#                       return(list(NULL))
+#                   }
+#               }
+#               mc.cores = ifelse(.Platform$OS.type == 'unix', as.integer(getArgs(obj)$CORES), 1) # if windows, can only use 1 here
+#               futile.logger::flog.info(paste("Running Sampling Using Parallel with ", getArgs(obj)$CORES, "Cores"))
+#               mcmc <- parallel::mclapply(seq_along(obj@cell_gene),
+#                                          FUN = par_func,
+#                                          mc.cores = mc.cores)
+#               return(mcmc)
+#           }
+# )
+
 setMethod(f="withParallel",
           signature="MCMC_inferCNV",
-          definition=function(obj)
-          {
+          definition=function(obj) {
               par_func <- function(i){
-                  if (getArgs(obj)$quietly == FALSE) {
-                      futile.logger::flog.info(paste("Sampling Number: ", i))
-                  }
-                  if(!(length(obj@cell_gene[[i]]$Cells) == 0)){
-                      tumor_grouping <- obj@group_id[ obj@cell_gene[[i]]$Cells ] # subset the tumor ids for the cells wanted
-                      gene_exp <- obj@expr.data[obj@cell_gene[[i]]$Genes, obj@cell_gene[[i]]$Cells]
-                      return(run_gibb_sampling(gene_exp, obj))
-                  } else {
-                      return(list(NULL))
-                  }
+                  tryCatch({
+                      if (getArgs(obj)$quietly == FALSE) {
+                          futile.logger::flog.info(paste("Starting processing for sampling number: ", i))
+                      }
+                      if(!(length(obj@cell_gene[[i]]$Cells) == 0)){
+                          futile.logger::flog.info(paste("Processing cells and genes at index: ", i))
+                          tumor_grouping <- obj@group_id[obj@cell_gene[[i]]$Cells]
+                          gene_exp <- obj@expr.data[obj@cell_gene[[i]]$Genes, obj@cell_gene[[i]]$Cells]
+                          futile.logger::flog.info(paste("Dimensions of gene expression data: ", dim(gene_exp)))
+                          result <- run_gibb_sampling(gene_exp, obj)
+                          futile.logger::flog.info(paste("Completed MCMC sampling for index: ", i))
+                          return(result)
+                      } else {
+                          futile.logger::flog.info(paste("No cells to process at index: ", i))
+                          return(list(NULL))
+                      }
+                  }, error=function(e){
+                      futile.logger::flog.error(paste("Error at index ", i, ": ", toString(e)))
+                      return(list(error=toString(e)))
+                  })
               }
-              mc.cores = ifelse(.Platform$OS.type == 'unix', as.integer(getArgs(obj)$CORES), 1) # if windows, can only use 1 here
-              futile.logger::flog.info(paste("Running Sampling Using Parallel with ", getArgs(obj)$CORES, "Cores"))
-              mcmc <- parallel::mclapply(seq_along(obj@cell_gene),
-                                         FUN = par_func,
-                                         mc.cores = mc.cores)
+
+              # Start with the maximum number of cores specified in the object's arguments
+              mc.cores <- ifelse(.Platform$OS.type == 'unix', as.integer(getArgs(obj)$CORES), 1)
+              mcmc <- NULL
+              successful <- FALSE
+
+              # Loop to decrement cores on failure
+              while(!successful && mc.cores >= 1) {
+                  tryCatch({
+                      mcmc <- parallel::mclapply(seq_along(obj@cell_gene),
+                                                 FUN = par_func,
+                                                 mc.cores = mc.cores)
+                      successful <- TRUE  # If mclapply succeeds, set successful to TRUE
+                  }, error=function(e){
+                      futile.logger::flog.error(paste("Error during parallel execution with ", mc.cores, " cores: ", toString(e)))
+                      mc.cores <- mc.cores - 5  # Decrement number of cores by 5
+                      mc.cores <- max(mc.cores, 1)  # Ensure cores do not fall below 1
+                  })
+              }
+
               return(mcmc)
           }
 )
+
 
 #' Run simulations in Non-Parallel mode
 #'
